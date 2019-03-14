ae98cf20b8ba105a1d8b92877c0a071d9471da30
sl <shenleiky@163.com>
2019-03-14 12:58:04 +0800

    [chap10][10.1][DSTACK]
    Signed-off-by: sl <shenleiky@163.com>

---
+++ chap10/LIFO-DSTACK.h
@@ -0,0 +1,61 @@
+#ifndef _LIFO_DSTACK_H_
+#define _LIFO_DSTACK_H_
+#include <stdlib.h>
+
+/* should add lock for pthread !!! */
+
+template <class T> 
+class DStack {
+public:
+	/*
+	 * @size, is the size of stack would be created
+	 */
+	DStack(int const size)
+	{
+		this->m_size = size;
+		this->m_stackp = (T *)malloc(sizeof(T) * size);
+		this->m_top = 0;
+	}
+	~DStack(void)
+	{
+		free(this->m_stackp);
+	}
+	
+	bool isEmpty(void) 
+	{
+		return this->m_top == 0;
+	}
+
+	bool isFull(void) 
+	{
+		return this->m_top >= this->m_size;
+	}
+	
+	int push(T *valp)
+	{
+		if (this->isFull())
+			return -1;
+		
+		this->m_stackp[this->m_top] = *valp;
+		this->m_top++;
+
+		return this->m_top;
+	}
+	int pop(T *valp)
+	{        
+		if (this->isEmpty())
+			return -1;
+		this->m_top--;
+		*valp = this->m_stackp[this->m_top];
+        
+        return this->m_top;
+	}
+	
+private:
+	T *m_stackp;
+	int m_top;
+	int m_size;
+};
+
+#endif
+
